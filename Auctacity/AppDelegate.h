95af680fd8edbe65f553541d9bde5835d13f9126
Jeremy Tharp <jeremy.tharp@nike.com>
2014-12-06 15:32:07 -0800

    Initial Commit

---
+++ Auctacity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auctacity
+//
+//  Created by Tharp, Jeremy on 12/6/14.
+//  Copyright (c) 2014 Tharp, Jeremy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Auctacity/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Auctacity
+//
+//  Created by Tharp, Jeremy on 12/6/14.
+//  Copyright (c) 2014 Tharp, Jeremy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
